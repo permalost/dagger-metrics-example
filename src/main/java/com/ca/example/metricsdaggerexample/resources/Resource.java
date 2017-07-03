364708b7edf9ab6a5c615d3fc443c757fa36a2f3
jhalvorson <jhalvorson@clearwateranalytics.com>
2017-07-03 10:01:01 -0600

    Cleanup of resource

--- src/main/java/com/ca/example/metricsdaggerexample/resources/Resource.java
+++ src/main/java/com/ca/example/metricsdaggerexample/resources/Resource.java
@@ -1,30 +1,16 @@
 
 package com.ca.example.metricsdaggerexample.resources;
 
-import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.annotation.Timed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.UriInfo;
 
 @Path("/")
 public class Resource {
 
-    @Context
-    UriInfo url;
-
-    @Context
-    Request request;
-
-    String name;
-	
-	MetricRegistry metricRegistry = new MetricRegistry();
-
 	@Timed
-    @GET
-    public String getTest() throws Exception
+	@GET
+	public String getTest() throws Exception
     {
 		return "TEST";
     }
